a1bcb98856bc7f5cc5ea18a377f3de9735762410
Tristram Jones <trisjones04@gmail.com>
2026-06-29 15:24:30 -0600

    adds basic controller and service

---
+++ src/main/java/com/kenect/contactsapi/contact/ContactController.java
@@ -0,0 +1,21 @@
+package com.kenect.contactsapi.contact;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ContactController {
+
+	private final ContactService contactService;
+
+	public ContactController(ContactService contactService) {
+		this.contactService = contactService;
+	}
+
+	@GetMapping("/contacts")
+	public List<Object> getAllContacts() {
+		return contactService.getAllContacts();
+	}
+}
---
+++ src/main/java/com/kenect/contactsapi/contact/ContactService.java
@@ -0,0 +1,13 @@
+package com.kenect.contactsapi.contact;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ContactService {
+
+	public List<Object> getAllContacts() {
+		return List.of();
+	}
+}
